c3247a165e6fe35ef7508501f1cfeb2bde18bfe4
MoneyMakinManiac <luthbell4@gmail.com>
2025-06-08 14:37:17 -0500

    add domains to CORS

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,12 @@ from flask_cors import CORS
 from scraper import scrape_nba_stats
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://sports-scraper-git-update-ludadons-projects.vercel.app"}})
+
+CORS(app, resources={r"/api/*": {"origins": [
+    "http://localhost:3000",
+    "https://sports-scraper.vercel.app",
+    "https://sports-scraper-git-search-text-ludadons-projects.vercel.app"
+]}})
 
 
 @app.route('/api/scrape')
