53cf76d7a6ba1da4438e65119152dc5f4569430b
MoneyMakinManiac <luthbell4@gmail.com>
2025-06-08 12:56:21 -0500

    updates cors endpoint

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from scraper import scrape_nba_stats
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/api/*": {"origins": "https://sports-scraper-git-update-ludadons-projects.vercel.app/"}})
 
 
 @app.route('/api/scrape')
